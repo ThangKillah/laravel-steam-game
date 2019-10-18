cecce236b9c0da18dd74a81a0ed317e69315b409
ThangBT <thangbt1307@gmail.com>
2019-10-18 15:08:13 +0700

    add gif loading
    Signed-off-by: ThangBT <thangbt1307@gmail.com>

--- resources/views/layout/blank.blade.php
+++ resources/views/layout/blank.blade.php
@@ -14,8 +14,12 @@
     <!-- theme css -->
     <link rel="stylesheet" href="{{ asset('css/theme.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    @stack('styles')
 </head>
 <body>
+<div id="loading">
+    <img id="loading-image" src="{{ asset('img/pacman.gif') }}" alt="Loading..." />
+</div>
 <!-- header -->
 <div class="fixed-header">
     @include('sub.menu')
@@ -41,5 +45,39 @@
 
 <!-- theme js -->
 <script src="{{ asset('js/theme.min.js') }}"></script>
+<script>
+    // $(window).on('load', function(){
+    //     $('#loading').fadeOut();
+    //     setTimeout(function(){ $('#loading').fadeOut(); }, 10000);
+    // });
+    // my callback function
+    // which relies on CSS being loaded function
+    function CSSDone() {
+        console.log('done loaded css');
+        $('#loading').fadeOut();
+        setTimeout(function(){ $('#loading').fadeOut(); }, 10000);
+    };
+
+    // load me some stylesheet
+    let url = "https://blog-game.com/css/theme.min.css",
+        head = document.getElementsByTagName('head')[0],
+        link = document.createElement('link');
+
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = url;
+
+    // MAGIC
+    // call CSSDone() when CSS arrives
+    head.appendChild(link);
+
+    link.onload = function () {
+        CSSDone('onload listener');
+    };
+</script>
+
+@stack('modal')
+@stack('js')
+
 </body>
 </html>
