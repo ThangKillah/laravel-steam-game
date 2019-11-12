204ccf59088dffecba23e9442f8da986edba7d34
ThangBT <thangbt1307@gmail.com>
2019-11-12 17:41:58 +0700

    init query sql
    Signed-off-by: ThangBT <thangbt1307@gmail.com>

--- app/Http/Controllers/GamingController.php
+++ app/Http/Controllers/GamingController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 use MarcReichel\IGDBLaravel\Models\Game;
-use MarcReichel\IGDBLaravel\Models\Platform;
 
 class GamingController extends Controller
 {
     public function test()
     {
-        $game =  Game::cache(0)->offset(0)->limit(100000)->get();
-        dd($game);
+        $games = Game::
+        whereHas('release_dates', function ($q) {
+            $q->whereIn('platform', [48, 49, 6]);
+        })
+            ->with(['platforms', 'websites', 'genres'])
+            ->offset(0)->limit(10)
+            ->whereDate('first_release_date', '>=', '2018-01-01')
+            ->where('category', 0)
+            ->get();
+        dd($games);
     }
 }
