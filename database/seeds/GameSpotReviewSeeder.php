823ef829ba2147a8ee5140f5ef6d0c5725d1e2f4
ThangBT <thangbt1307@gmail.com>
2019-12-03 15:31:20 +0700

    add api gamespot + rating jquery
    Signed-off-by: ThangBT <thangbt1307@gmail.com>

--- database/seeds/GameSpotReviewSeeder.php
+++ database/seeds/GameSpotReviewSeeder.php
@@ -14,6 +14,8 @@ class GameSpotReviewSeeder extends Seeder
 
     public function run()
     {
+        DB::table('gamespot_reviews')->truncate();
+
         for ($i = 1; $i <= 10000; $i++) {
             $reviews = $this->getReview($i);
             if (!empty($reviews)) {
@@ -22,25 +24,27 @@ class GameSpotReviewSeeder extends Seeder
                 } else {
                     $data = [];
                     foreach ($reviews['results'] as $item) {
-                        $insertData = [
-                            'id_review' => $item['id'],
-                            'authors' => $item['authors'],
-                            'title' => $item['title'],
-                            'deck' => $item['deck'],
-                            'body' => $item['body'],
-                            'good' => $item['good'],
-                            'bad' => $item['bad'],
-                            'publish_date' => $item['publish_date'],
-                            'edit_date' => $item['update_date'],
-                            'score' => $item['score'],
-                            'review_type' => $item['review_type'],
-                            'site_detail_url' => $item['site_detail_url'],
-                            'image' => json_encode($item['image']),
-                            'game' => empty($item['game']) ? null : explode("/", $item['game']["site_detail_url"])[3],
-                            'created_at' => now(),
-                            'updated_at' => now()
-                        ];
-                        array_push($data, $insertData);
+                        if (!empty($item['game'])) {
+                            $insertData = [
+                                'id_review' => $item['id'],
+                                'authors' => $item['authors'],
+                                'title' => $item['title'],
+                                'deck' => $item['deck'],
+                                'body' => $item['body'],
+                                'good' => $item['good'],
+                                'bad' => $item['bad'],
+                                'publish_date' => $item['publish_date'],
+                                'edit_date' => $item['update_date'],
+                                'score' => $item['score'],
+                                'review_type' => $item['review_type'],
+                                'site_detail_url' => $item['site_detail_url'],
+                                'image' => json_encode($item['image']),
+                                'game' => explode("/", $item['game']["site_detail_url"])[3],
+                                'created_at' => now(),
+                                'updated_at' => now()
+                            ];
+                            array_push($data, $insertData);
+                        }
                     }
                     DB::table('gamespot_reviews')->insert($data);
                 }
