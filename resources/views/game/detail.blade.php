9269c36365e1bae2128687282e0a3759279017fc
ThangBT <thangbt1307@gmail.com>
2020-01-06 13:34:08 +0700

    update image screenshots css
    Signed-off-by: ThangBT <thangbt1307@gmail.com>

--- resources/views/game/detail.blade.php
+++ resources/views/game/detail.blade.php
@@ -295,39 +295,14 @@
             //init slide img
             $(".slicker-show-up").slick({
                 autoplay: true,
-                centerMode: true,
                 autoplaySpeed: 4000,
                 variableWidth: true,
                 lazyLoad: "progressive",
                 dots: true,
-                responsive: [
-                    {
-                        breakpoint: 1024,
-                        settings: {
-                            slidesToShow: 3,
-                            slidesToScroll: 3,
-                            infinite: true,
-                            //dots: true
-                        }
-                    },
-                    {
-                        breakpoint: 600,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 2
-                        }
-                    },
-                    {
-                        breakpoint: 480,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1
-                        }
-                    },
-                    // You can unslick at a given breakpoint now by adding:
-                    // settings: "unslick"
-                    // instead of a settings object
-                ],
+                centerMode: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
             });
 
             //init slide video
